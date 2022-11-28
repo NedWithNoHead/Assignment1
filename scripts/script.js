6f17ed97f973621792fa8450c09f8476b754c82b
Tin_Le <jackletrungtin@gmail.com>
2022-11-28 07:53:41 -0800

    added working cancel button, make new note button display hidden area

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,31 @@
 
-const DarkThemeButton = document.querySelector('.theme.button')
-const AsideArea = document.querySelector('aside')
+const DarkThemeButton = document.querySelector('.theme.button');
+const AsideArea = document.querySelector('aside');
+const CancelButton = document.querySelector('.cancel.button');
+const HideSave = document.querySelector('.save.button');
+const HideText = document.querySelector('.myTextBox');
+const NewNote = document.querySelector('.newnote.button')
+
 
 const TriggerDarkTheme = function(){
     document.body.classList.toggle('darktheme');
-    AsideArea.classList.toggle('darkaside')
+    AsideArea.classList.toggle('darkaside');
 }
 
-DarkThemeButton.addEventListener('click', TriggerDarkTheme)
+DarkThemeButton.addEventListener('click', TriggerDarkTheme);
+
+const HideTextArea = function (){
+    HideSave.classList.toggle('DeleteAll');
+    CancelButton.classList.toggle('DeleteAll');
+    HideText.classList.toggle('DeleteAll');
+}
+
+CancelButton.addEventListener('click', HideTextArea);
+
+const MakeNewNote = function(){
+    HideSave.classList.toggle('DeleteAll');
+    CancelButton.classList.toggle('DeleteAll');
+    HideText.classList.toggle('DeleteAll');
+}
+
+NewNote.addEventListener('click', MakeNewNote)
