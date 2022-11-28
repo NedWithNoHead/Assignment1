6e2ef2579d21ddebbd42654c9b30a5b7503a6218
Tin_Le <jackletrungtin@gmail.com>
2022-11-27 20:39:55 -0800

    added a working dark theme button. feels good

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 
 const DarkThemeButton = document.querySelector('.theme.button')
+const AsideArea = document.querySelector('aside')
 
 const TriggerDarkTheme = function(){
-    document.body.classList.add('darktheme');
+    document.body.classList.toggle('darktheme');
+    AsideArea.classList.toggle('darkaside')
 }
 
 DarkThemeButton.addEventListener('click', TriggerDarkTheme)
